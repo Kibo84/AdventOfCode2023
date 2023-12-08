ee54d6f157f1001b96fe49805ff0af2fccb76f15
Kibo84 <100001143+Kibo84@users.noreply.github.com>
2023-12-09 00:30:42 +0100

    minor refactor

--- src/main/kotlin/challenges/day8/second/SecondChallenge.kt
+++ src/main/kotlin/challenges/day8/second/SecondChallenge.kt
@@ -14,10 +14,10 @@ const val origin = 'A'
 fun main() {
     var instructions = ""
     val file = File("src/inputs/input-day-8.txt")
-
     val lines = BufferedReader(InputStreamReader(file.inputStream())).use { fileReader ->
         fileReader.readLines()
     }
+
     lines.filter { it.isNotEmpty() }.forEach {
         if (it.contains(regexInstructions)) {
             instructions = it
@@ -28,53 +28,59 @@ fun main() {
         }
     }
     val nodeRoutes = nodeMap.values.filter(Node::isOrigin)
-    val result = calculateLeastCommonMultipleOfList(calculateAllRoutes(instructions, nodeRoutes))
+    val result = calculateLeastCommonMultipleOfList(calculateAllRoutesSteps(instructions, nodeRoutes))
 
     println(result)
 }
 
-fun calculateAllRoutes(instructions: String, nodeList: List<Node>): List<Long> {
-    return nodeList.map { calculateNodeRout(instructions, it) }
+fun calculateAllRoutesSteps(instructions: String, nodeList: List<Node>): List<Long> {
+    return nodeList.map { calculateRoutSteps(instructions, it) }
 }
 
-fun calculateNodeRout(instructions: String, node: Node): Long {
+fun calculateRoutSteps(instructions: String, node: Node): Long {
     var result = 0L
     var repeat = 0
     var tempNode = node.copy()
-    while (result == 0L) {
+    while (result.isZero()) {
         instructions.forEachIndexed { index, instruction ->
-            tempNode = nextNode(tempNode, instruction)
+            tempNode = searchNextNode(tempNode, instruction)
             if (Node.arriveToDestiny(tempNode)) result = (index + indexAdjustment).toLong()
         }
-        if (result != 0L) result += (instructions.length * repeat)
+        if (!result.isZero()) result += (instructions.length * repeat)
         repeat++
     }
     return result
 }
 
-fun nextNode(node: Node, instruction: Char): Node {
+fun searchNextNode(node: Node, instruction: Char): Node {
     return if (instruction == leftInstruction) nodeMap[node.nextLeft]!! else nodeMap[node.nextRight]!!
 }
 
-fun calculateLeastCommonMultipleOfList(numbers: List<Long>): Long {
-    var result = numbers[0]
-    numbers.forEachIndexed { index, _ ->
-        result = calculateLeastCommonMultiple(result, numbers[index])
+fun calculateLeastCommonMultipleOfList(routesSteps: List<Long>): Long {
+    val firstIndex = 0
+    var result = routesSteps[firstIndex]
+    routesSteps.forEachIndexed { index, _ ->
+        result = calculateLeastCommonMultiple(result, routesSteps[index])
     }
 
     return result
 }
 
 fun calculateGreatestCommonDivisor(numberA: Long, numberB: Long): Long {
-    return if (numberB == 0L) numberA else calculateGreatestCommonDivisor(numberB, numberA % numberB)
+    return if (numberB.isZero()) numberA else calculateGreatestCommonDivisor(numberB, numberA % numberB)
 }
 
 fun calculateLeastCommonMultiple(numberA: Long, numberB: Long): Long {
-    return if (numberA == 0L || numberB == 0L) {
-        0L
+    val commonMultipleWithZero = 0L
+    return if (numberA.isZero() || numberB.isZero()) {
+        commonMultipleWithZero
     } else {
         numberA * numberB / calculateGreatestCommonDivisor(numberA, numberB)
     }
+}
+
+fun Long.isZero(): Boolean{
+    return this == 0L
 }
 
 data class Node(val nodeName: String, val nextLeft: String, val nextRight: String) {
